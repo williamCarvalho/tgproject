5608cb5d953ea80a81c6bfc2e374e27d93410630
WillCarvalho <william.melise@gmail.com>
2017-10-10 11:45:44 -0300

    processo funcional

--- blog/forms.py
+++ blog/forms.py
@@ -1,15 +1,13 @@
 from django import forms
 
 from .models import Post
-<<<<<<< HEAD
+
 from .models import Consulta
-=======
->>>>>>> 3dc1fb21c8ebb663bac49ad2a7efbe2c54ac7f47
 
 class PostForm(forms.ModelForm):
     
     class Meta:
-<<<<<<< HEAD
+
         #model = Post
         #fields = ('title', 'text', 'author')
         
@@ -20,7 +18,3 @@ class PostForm(forms.ModelForm):
             "data_inicio":forms.TextInput(attrs={'type':'date','class':'form-control'}),
             "data_fim":forms.TextInput(attrs={'type':'date','class':'form-control'}),
         }  
-=======
-        model = Post
-        fields = ('title', 'text')
->>>>>>> 3dc1fb21c8ebb663bac49ad2a7efbe2c54ac7f47
--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     url(r'^$', views.consulta, name='consulta'),
     url(r'^resultado/$', views.resultado, name='resultado'),
     url(r'^lista/$', views.post_list, name='post_list'),
@@ -10,10 +9,4 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^charts/chart.png$', views.simple, name='chart'),
-=======
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
->>>>>>> 3dc1fb21c8ebb663bac49ad2a7efbe2c54ac7f47
 ]
